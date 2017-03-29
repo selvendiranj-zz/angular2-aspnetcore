f222fe3fd91a5dc9ae46a648de02f921c4ba4212
selvendiranj <selvendiranj@gmail.com>
2017-03-29 20:18:26 +0530

    project rename

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WebApplication2
+namespace Web
 {
     public class Startup
     {
